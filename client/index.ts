d849b48aa45cf9391e7a96b3acbcafaf015f1990
Anillc <i@anillc.cn>
2023-07-23 20:44:39 +0800

    fix: import page directly

--- client/index.ts
+++ client/index.ts
@@ -1,11 +1,12 @@
 import { defineExtension } from '@koishijs/client'
+import Downloads from './downloads.vue'
 import './icons'
 
-export default defineExtension(ctx => {
+export default defineExtension(async ctx => {
   ctx.page({
     name: '下载',
     path: '/downloads',
     icon: 'activity:download',
-    component: () => import('./downloads.vue')
+    component: Downloads,
   })
 })
